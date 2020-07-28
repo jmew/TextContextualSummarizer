956ab404ffb8e1a1dfc76113c0d4f1a0b44f3bbb
Sid Unnithan <sidunnithan@gmail.com>
2020-07-28 11:42:39 -0700

    added connection adapter for service testing

--- test_service.py
+++ test_service.py
@@ -4,7 +4,7 @@ import requests as rq
 if __name__ == "__main__":
     files = {'transcript': open('transcript.vtt', 'rb')}
 
-    response = rq.post(f"localhost:5000/summarize", files=files)
+    response = rq.post(f"http://localhost:5000/summarize", files=files)
 
     if response.status_code != 200:
         print("Invalid response...")
