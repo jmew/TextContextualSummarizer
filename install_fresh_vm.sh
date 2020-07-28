0d404a71ec1ac500218a69c1a6cbf07f6ad71a98
Sid Unnithan <sidunnithan@gmail.com>
2020-07-28 11:35:40 -0700

    Moved package install into VM install script

--- install_fresh_vm.sh
+++ install_fresh_vm.sh
@@ -3,3 +3,5 @@ sudo apt-get update
 sudo apt install python3-pip
 
 sudo apt install python3-flask
+
+pip3 install -r requirements.txt
--- start_remote_server.sh
+++ start_remote_server.sh
@@ -1,5 +1,3 @@
-pip3 install -r requirements.txt
-
 export FLASK_APP=remote_server.py
 
 flask run --host=0.0.0.0
